219794fc264fd3da9fc91f0b31b9b03751d36a42
Lars Moan <lars.ostberg.moan@gmail.com>
2022-04-04 09:46:40 +0200

    fix typecasted memory

--- 2_uart/uart.c
+++ 2_uart/uart.c
@@ -1,13 +1,30 @@
 #include "uart.h"
 
+
+
 void uart_init(){
     //UART_INT_RX
     GPIO0->PIN_CNF[6] = 0;
     //UART_INT_TX
     GPIO1->PIN_CNF[8] = 1;
 
+    //Gjøre noe med PSELTXD og RXD, what to do?
+
+
+
     //Mulig den skal sette enable etc
-    UART0->ENABLE[??] = 8;  //Usikker på pinne for ENABLE register??
+    UART0->ENABLE = 8;  //Usikker på pinne for ENABLE register??
+
+   
+    //Sette baudrate
+    UART0->BAUDRATE = 0x00275000;
+
+    //Disconnecting
+    UART0->PSEL_RTS = 1;
+    UART0->PSEL_CTS = 1;
+
+    //Eget enable register?
+    UART0->TASKS_STARTRX = 1;
 
     
 }
